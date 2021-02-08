6915f49bb7ab74edf3ea09225b66577227a76645
Rudi <rudixlab@gmail.com>
2021-02-08 22:54:50 +0200

    ddd

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,11 @@ import React from 'react'
 
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
-    <div className="logo">
-      <span className="icon fa-diamond"></span>
-    </div>
+    <div className="logo">dsdsd1</div>
     <div className="content">
       <div className="inner">
-        <h1>Dimension</h1>
-        <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
-          <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
-        </p>
+        <h1>Melisa Laurini</h1>
+        <p>beautician</p>
       </div>
     </div>
     <nav>
